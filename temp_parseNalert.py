aea2f35257f269849d44f72ed93daedaf6a08675
dev <dev@penguinbeam>
2018-03-18 15:26:44 +0000

    Fetch sensor names/serials from temp log and use them in ifttt call

--- temp_parseNalert.py
+++ temp_parseNalert.py
@@ -4,6 +4,7 @@ import requests, json
 import time
 
 import os
+from os import environ
 
 iftttMakerKey = os.environ["IFTTTKEY"]
 
@@ -27,25 +28,39 @@ with open(file) as myfile:
 urlPart1 = 'https://maker.ifttt.com/trigger/'
 urlPart3 = '/with/key/' + iftttMakerKey
 
-for x in range(5, 6):
-    low = float(json.loads(lowDataLine)['tort' + str(x)])
-    high = float(json.loads(highDataLine)['tort' + str(x)])
-    temp = float(json.loads(tempDataLine)['tort' + str(x)])
+#Extract all sensor names/serial numbers from log
+sensorList=[]
+data = json.loads(tempDataLine)
+data.pop('timestamp', 0)
+for key, value in data.iteritems():
+    temp = [key,value]
+    sensorList.append(temp)
+
+#For each sensor look up its value from the log data
+for x in range(0, len(sensorList)):
+    sensorKey = str(sensorList[x][0])
+    high = float(json.loads(highDataLine)[sensorKey])
+    temp = float(json.loads(tempDataLine)[sensorKey])
+    low = float(json.loads(lowDataLine)[sensorKey])
     if (temp > high):
         #Send an IFTTT alert
-        url = urlPart1 + 'tort' + str(x) + '_over_temp' + urlPart3
-        values = {'value1' : 'tort' + str(x), 'value2' : temp, 'value3' : high}
+        url = urlPart1 + sensorKey + '_over_temp' + urlPart3
+        values = {'value1' : sensorKey, 'value2' : temp, 'value3' : high}
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
         requestResponse = requests.post(url, data=json.dumps(values), headers=headers)
         print(requestResponse)
-        print(request)
+        #print(values)
         time.sleep(5)
     elif (temp < low):
         #Send an IFTTT alert
-        url = urlPart1 + 'tort' + str(x) + '_under_temp' + urlPart3
-        values = {'value1' : 'tort' + str(x), 'value2' : temp, 'value3' : low}
+        url = urlPart1 + sensorKey + '_under_temp' + urlPart3
+        values = {'value1' : sensorKey, 'value2' : temp, 'value3' : low}
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
         requestResponse = requests.post(url, data=json.dumps(values), headers=headers)
         print(requestResponse)
+        #print(values)
         time.sleep(5)
+   # else:
+   #     values = {'value1' : 'tort' + str(x), 'value2' : temp, 'value3' : low}
+   #     print(values)
 
