52da7f90410b5fd9ba515166824596cc26eafcc3
dev <dev@penguinbeam>
2018-10-15 22:01:59 +0000

    Make things work from an initial setup because some config was missing!

--- setup.sh
+++ setup.sh
@@ -1,8 +1,32 @@
+
+sudo apt-get update
+sudo apt-get install -y git
+sudo git clone https://github.com/penguinbeam/ISOtemp
+
+sudo apt-get install -y python-requests
+sudo apt-get install -y python-bluez
+
+#Install dependancy for the MI home trigger
 sudo apt-get install python-requests
-sudo apt-get install python-bluez
 
 sudo /sbin/modprobe w1-gpio
 sudo /sbin/modprobe w1-therm
 
-On bluetooth receiving device make discoverable
-sudo hciconfig hci0 piscan
+echo "w1-gpio" | sudo tee -a /etc/modules
+echo "w1-therm" | sudo tee -a /etc/modules
+
+echo "dtoverlay=w1-gpio" | sudo tee -a /boot/config.txt
+
+echo "These are your devices, if nothing shows reboot for the dtoverlay setting to take effect"
+ls /sys/bus/w1/devices/ | grep -e "[0-9abcdef][0-9abcdef]-[0-9abcdef]\+"
+
+sudo apt-get install -y supervisor
+sudo cp /opt/ISOtemp/supervisord.conf.example /etc/supervisor/conf.d/isotemp.conf
+
+echo "Find out BD Address for collecting server"
+echo "sudo hciconfig hci0"
+echo "On bluetooth receiving device make discoverable"
+echo "sudo hciconfig hci0 piscan"
+
+
+echo "Now edit your supervisor config isotemp.conf filling in the environment variables, remove the block not needed (IE no receiver on the logger, no transmitter on the receiver)"
